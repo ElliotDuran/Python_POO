912cafe1026d39aedd385db7b20e139d62dca8cf
ElliotDM <elliotd70@gmail.com>
2020-08-19 11:59:52 -0500

    New method has been created

--- cash_register.py
+++ cash_register.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 class CashRegister():
-    """ A simple class that represents the contents of a cash register """
+    """ A simple class that represents the contents of a cash register. """
 
     def __init__(self, name, dollars, quartes, dimes):
         if is_number(dollars) and is_number(quartes) and is_number(dimes):
@@ -19,8 +19,13 @@ class CashRegister():
     def total(self):
         return (self.dimes * 0.1) + (self.quartes * 0.25) + self.dollars
 
-    def __gt__(self, other):
+    def __lt__(self, other): 
+        """ This method return the list from the highest to the lowest. """
         return self.total() > other.total()
+
+    # def __gt__(self, other):
+    #     """ This method return the list from the lowest to the highest. """
+    #     return self.total() > other.total()
 
     def __str__(self):
         return self.name + ": there are " + str(self.dollars) + " dollars, " + \
@@ -33,7 +38,7 @@ def is_number(value):
 
 
 def not_empty_string(string):
-    if len(string) is not 0:
+    if len(string) != 0:
         return True
 
 
@@ -42,10 +47,11 @@ if __name__ == "__main__":
     c2 = CashRegister("Cash register 02", 3, 11, 8)     # $6.55
     c3 = CashRegister("Cash register 03", 10, 1, 2)     # $10.45
     c4 = CashRegister("Cash register 04", 2, 6, 1)      # $3.60
-    c5 = CashRegister("Cash register 05", 13, 9, 14)    # $10.9
+    c5 = CashRegister("Cash register 05", 13, 9, 14)    # $18.9
     
     cash_list = [c1, c2, c3, c4, c5]
     sorted_cash_list = sorted(cash_list)
+    print()
 
     for i in sorted_cash_list:
-        print(i)
+        print(i, end='\n\n')
